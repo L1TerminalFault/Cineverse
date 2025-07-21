dda0901f357403bb6e0cfcc740a803ccc9b4e2a9
Error <null>
2025-07-22 00:48:40 +0300

    progress

--- src/app/(main)/explore/layout.jsx
+++ src/app/(main)/explore/layout.jsx
@@ -1,7 +1,13 @@
+import { Suspense } from "react";
+
 export const metadata = {
   title: "Explore",
 };
 
 export default function ({ children }) {
-  return <div className="min-h-screen bg-[#020409] w-full">{children}</div>;
+  return (
+    <Suspense>
+      <div className="min-h-screen bg-[#020409] w-full">{children}</div>;
+    </Suspense>
+  );
 }
