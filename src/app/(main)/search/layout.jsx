060b2015066deec06b1eab582d2f2d21b1b57cf5
Error <null>
2025-07-22 01:14:17 +0300

    progress

--- src/app/(main)/search/layout.jsx
+++ src/app/(main)/search/layout.jsx
@@ -1,7 +1,13 @@
+import { Suspense } from "react";
+
 export const metadata = {
   title: "Search",
 };
 
 export default function ({ children }) {
-  return <div className="min-h-screen bg-[#020409] w-full">{children}</div>;
+  return (
+    <Suspense fallback={<div></div>}>
+      <div className="min-h-screen bg-[#020409] w-full">{children}</div>;
+    </Suspense>
+  );
 }
