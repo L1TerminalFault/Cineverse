7983e370abef0193a9144f5c8c3a3ec1cd4961f9
Error <null>
2025-08-17 04:53:27 +0300

    progress

--- src/app/(main)/home/layout.jsx
+++ src/app/(main)/home/layout.jsx
@@ -1,11 +1,13 @@
+import { Suspense } from "react";
+
 export const metadata = {
-	title: "Home"
-}
+  title: "Home",
+};
 
 export default function ({ children }) {
-	return (
-		<div className="w-full h-full">
-			{children}
-		</div>
-	)
+  return (
+    <Suspense fallback={<div></div>}>
+      <div className="w-full h-full">{children}</div>
+    </Suspense>
+  );
 }
